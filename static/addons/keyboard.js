fa41d7e2ae785bd61593498d435e2b74a959aa92
OtherExit <138784480+OtherExit@users.noreply.github.com>
2023-07-28 18:42:55 +0000

    Updating keyboard.js

--- static/addons/keyboard.js
+++ static/addons/keyboard.js
@@ -1,9 +1,12 @@
 // This addon lets you add keyboard shortcuts to OtherApps
+// The copy and paste shortcuts are edited in "copypaste.js"
 
 document.addEventListener('keydown', e => {
     // Save
-    if (e.ctrlKey && e.key === 's')
+    if (e.ctrlKey && e.key === 's'){
+        e.preventDefault();
         save();
+    }
 
     // Test the app
     if (e.key == 'F5') {
